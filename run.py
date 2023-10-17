54b3f8c42f2efcda117dfac50c5f89d55b514332
jfpaliga <>
2023-10-17 18:23:43 +0000

    Add object class and function for player to examine an object

--- run.py
+++ run.py
@@ -2,6 +2,21 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+ROOM_OBJECTS = {
+    "door": "A sturdy oak door locked with a solid padlock",
+    "padlock": "A solid steel 4-digit combination lock",
+    "casket": "A wooden casket with a slightly rotted lid",
+    "book": "An old tome that is sealed with a standard lock"
+}
+
+
+class Object:
+    def __init__(self, description):
+        self.description = description
+
+    def describe(self):
+        print(self.description)
+
 
 def title():
     title = r'''
@@ -23,14 +38,23 @@ def initiate():
     print("You see a door with a padlock, a closed casket and a book.\n")
     print("To see more details, type examine e.g. 'examine door'")
     print("To use an object, type use e.g. 'use door'")
-    print("To see these instructions again, type 'help'")
+    print("To see these instructions again, type 'help'\n")
     action = input()
     player_action(action)
 
 
+def examine(item):
+    room_item = Object(ROOM_OBJECTS[item])
+    return room_item.describe()
+
+
 def player_action(action):
-    if action.lower() == "help":
+    player_input = action.split(' ')
+
+    if player_input[0].lower() == "help":
         initiate()
+    elif player_input[0].lower() == "examine":
+        examine(player_input[1].lower())
     else:
         print("Enter a valid input.")
         new_action = input()
