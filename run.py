37ae1a05789408cc19793384672b1c267f241940
jfpaliga <>
2023-10-17 17:56:35 +0000

    Add game initiation and player input function

--- run.py
+++ run.py
@@ -15,11 +15,31 @@ def title():
             |___/|_|                            |_|         
             '''
     print(title + "\n")
-    print("Welcome to Crypt Escape!")
+    print("Welcome to Crypt Escape!\n")
+
+
+def initiate():
+    print("You are in a cold stone crypt, a single torch provides light.")
+    print("You see a door with a padlock, a closed casket and a book.\n")
+    print("To see more details, type examine e.g. 'examine door'")
+    print("To use an object, type use e.g. 'use door'")
+    print("To see these instructions again, type 'help'")
+    action = input()
+    player_action(action)
+
+
+def player_action(action):
+    if action.lower() == "help":
+        initiate()
+    else:
+        print("Enter a valid input.")
+        new_action = input()
+        player_action(new_action)
 
 
 def main():
     title()
+    initiate()
 
 
 main()
