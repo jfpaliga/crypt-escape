3c0198d8a8c64a86a6401a33eb3226cd4098c965
jfpaliga <>
2023-10-17 17:36:06 +0000

    Set up basic running functions and add title ascii art

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def title():
+    title = r'''
+ _____                  _     _____                         
+/  __ \                | |   |  ___|                        
+| /  \/_ __ _   _ _ __ | |_  | |__ ___  ___ __ _ _ __   ___ 
+| |   | '__| | | | '_ \| __| |  __/ __|/ __/ _` | '_ \ / _ \
+| \__/\ |  | |_| | |_) | |_  | |__\__ \ (_| (_| | |_) |  __/
+ \____/_|   \__, | .__/ \__| \____/___/\___\__,_| .__/ \___|
+             __/ | |                            | |         
+            |___/|_|                            |_|         
+            '''
+    print(title + "\n")
+    print("Welcome to Crypt Escape!")
+
+
+def main():
+    title()
+
+
+main()
