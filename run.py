ac3dc775aac9bc52477f0b6013291812699b80d0
jfpaliga <>
2023-10-21 13:57:06 +0000

    Add player inventory functionality and refactor player action code

--- run.py
+++ run.py
@@ -3,25 +3,27 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 ROOM_ITEMS = {
-    "door": "A sturdy oak door locked with a solid padlock",
-    "padlock": "A solid steel 4-digit combination lock",
-    "casket": "A wooden casket with a slightly rotted lid",
-    "book": "An old tome that is sealed with a standard lock",
-    "skeleton": '''It's an old skeleton. Something reflects the torchlight 
-    from inside it's mouth.'''
+    "door": "A sturdy oak door locked with a solid padlock.\n",
+    "padlock": "A solid steel 4-digit combination lock.\n",
+    "casket": "A wooden casket with a slightly rotted lid.\n",
+    "book": "An old tome that is sealed with a standard lock.\n",
+    "skeleton": '''It's an old skeleton. Something reflects the torchlight
+     from inside it's mouth.\n'''
 }
 
 ITEM_USES = {
-    "door": "The door doesn't budge.",
-    "casket": "You remove the lid and inside the casket is a skeleton.",
-    "book": "The book cannot be opened without a key.",
-    "skeleton": "You open the mouth of a skeleton and take a key from inside.",
-    "padlock": "Placeholder"
+    "door": "The door doesn't budge.\n",
+    "casket": "You remove the lid and inside the casket is a skeleton.\n",
+    "book": "You pick up the book. It cannot be opened without a key.\n",
+    "skeleton": '''You open the mouth of a skeleton and
+     take a key from inside.\n''',
+    "padlock": "Placeholder\n"
 }
 
 
 class Item:
     def __init__(self, item):
+        self.name = item
         self.description = ROOM_ITEMS[item]
         self.use = ITEM_USES[item]
         self.is_locked = True
@@ -36,6 +38,9 @@ class Item:
 class Player:
     def __init__(self):
         self.inventory = []
+ 
+    def add_to_inventory(self, item):
+        self.inventory.append(item)
 
 
 def title():
@@ -53,15 +58,18 @@ def title():
     print("Welcome to Crypt Escape!\n")
 
 
-def initiate():
-    player = Player
+def initiate(player):
     print("You are in a cold stone crypt, a single torch provides light.")
     print("You see a door with a padlock, a closed casket and a book.\n")
     print("To see more details, type examine e.g. 'examine door'")
     print("To use an object, type use e.g. 'use door'")
-    print("To see these instructions again, type 'help'\n")
+    print("To check your inventory, type inventory")
+    print("To see these instructions again, type help\n")
+
+
+def request_action(player):
     action = input()
-    player_action(player, action)
+    validate_action(player, action)
 
 
 def examine(item):
@@ -69,41 +77,55 @@ def examine(item):
         room_item = Item(item)
         return room_item.describe()
     else:
-        print("There is nothing to see.")
+        print("There is nothing to see.\n")
 
 
 def validate_use(player, item):
     if item in ITEM_USES.keys():
         room_item = Item(item)
-        return room_item.use_object()
+        use(player, room_item)
     else:
-        print("You cannot use that.")
+        print("You cannot use that.\n")
 
 
-def player_action(player, action):
+def use(player, item):
+    if item.name == "key" or "book":
+        player.add_to_inventory(item.name)
+    return player, item.use_object()
+
+
+def check_inventory(player):
+    if len(player.inventory) == 0:
+        print("You have nothing in your inventory.")
+    else:
+        print("You have the following items in your inventory:\n")
+        for item in player.inventory:
+            print(item)
+
+
+def validate_action(player, action):
     player_input = action.split(' ')
 
     if player_input[0].lower() == "help":
-        initiate()
+        initiate(player)
+    elif player_input[0].lower() == "inventory":
+        check_inventory(player)
+        request_action(player)
     elif player_input[0].lower() == "examine":
         examine(player_input[1].lower())
-        print("")
-        new_action = input()
-        player_action(player, new_action)
+        request_action(player)
     elif player_input[0].lower() == "use":
         validate_use(player, player_input[1].lower())
-        print("")
-        new_action = input()
-        player_action(player, new_action)
+        request_action(player)
     else:
         print("Enter a valid input.\n")
-        new_action = input()
-        player_action(player, new_action)
+        request_action(player)
 
 
 def main():
+    player = Player()
     title()
-    initiate()
+    initiate(player)
 
 
 main()
