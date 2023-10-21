a465ec0d6bddc03b3cdc531e30feb1a315762d84
jfpaliga <>
2023-10-21 13:03:11 +0000

    Add player action validation

--- run.py
+++ run.py
@@ -12,19 +12,19 @@ ROOM_ITEMS = {
 }
 
 ITEM_USES = {
-    "locked door": "The door doesn't budge.",
-    "unlocked door": "The door swings open, revealing the way out!",
+    "door": "The door doesn't budge.",
     "casket": "You remove the lid and inside the casket is a skeleton.",
-    "locked book": "The book cannot be opened without a key.",
-    "unlocked book": "The lock comes free allowing you to read the book.",
-    "skeleton": "You open the mouth of a skeleton and take a key from inside."
+    "book": "The book cannot be opened without a key.",
+    "skeleton": "You open the mouth of a skeleton and take a key from inside.",
+    "padlock": "Placeholder"
 }
 
 
 class Item:
     def __init__(self, item):
         self.description = ROOM_ITEMS[item]
-        self.use = None
+        self.use = ITEM_USES[item]
+        self.is_locked = True
 
     def describe(self):
         print(self.description)
@@ -54,26 +54,33 @@ def title():
 
 
 def initiate():
+    player = Player
     print("You are in a cold stone crypt, a single torch provides light.")
     print("You see a door with a padlock, a closed casket and a book.\n")
     print("To see more details, type examine e.g. 'examine door'")
     print("To use an object, type use e.g. 'use door'")
     print("To see these instructions again, type 'help'\n")
     action = input()
-    player_action(action)
+    player_action(player, action)
 
 
 def examine(item):
-    room_item = Item(item)
-    return room_item.describe()
+    if item in ROOM_ITEMS.keys():
+        room_item = Item(item)
+        return room_item.describe()
+    else:
+        print("There is nothing to see.")
 
 
-def use(item):
-    room_item = Item(item)
-    return room_item.use_object()
+def validate_use(player, item):
+    if item in ITEM_USES.keys():
+        room_item = Item(item)
+        return room_item.use_object()
+    else:
+        print("You cannot use that.")
 
 
-def player_action(action):
+def player_action(player, action):
     player_input = action.split(' ')
 
     if player_input[0].lower() == "help":
@@ -82,16 +89,16 @@ def player_action(action):
         examine(player_input[1].lower())
         print("")
         new_action = input()
-        player_action(new_action)
+        player_action(player, new_action)
     elif player_input[0].lower() == "use":
-        use(player_input[1].lower())
+        validate_use(player, player_input[1].lower())
         print("")
         new_action = input()
-        player_action(new_action)
+        player_action(player, new_action)
     else:
         print("Enter a valid input.\n")
         new_action = input()
-        player_action(new_action)
+        player_action(player, new_action)
 
 
 def main():
