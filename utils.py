bc5434c9ff5abf6209ed9c3d6e5e86332e38aeb3
jfpaliga <>
2023-11-06 17:43:31 +0000

    Add docstrings to utils.py functions

--- utils.py
+++ utils.py
@@ -2,6 +2,19 @@ from constants import ROOM_ITEMS, ITEM_USES
 
 
 def validate_action(player, action):
+    """
+    Take player input and split it into a list. Check the list
+    for keywords and execute an appropriate Player class method.
+
+    After calling the class method, call the Player.get_action()
+    method to get another input from the player to continue the
+    game.
+
+    Returns
+    -------
+    None
+    """
+
     from run import Item
     player_input = action.split(' ')
     choice = player_input[0].lower()
@@ -25,6 +38,21 @@ def validate_action(player, action):
 
 
 def validate_use(player, item):
+    """
+    Take a passed string and check if it exists as a key in the
+    ITEM_USES dictionary.
+
+    If it does, instantiate item as an Item object and call the
+    Player.use() class method using the new Item object.
+
+    Otherwise, print a statement to inform the player they cannot
+    use that item.
+
+    Returns
+    -------
+    None
+    """
+
     from run import Item
     if item in ITEM_USES.keys():
         room_item = Item(item, ROOM_ITEMS[item], ITEM_USES[item])
@@ -34,6 +62,21 @@ def validate_use(player, item):
 
 
 def validate_code(player, code):
+    """
+    Pass player input through a try/except loop to validate that the
+    input is an integer. Within the try loop, check that the integer
+    is 4 digits long.
+
+    If both cases pass, the use_padlock() function is called.
+
+    Otherwise, ask for a valid input and pass the new input
+    back into this function.
+
+    Returns
+    -------
+    None
+    """
+
     try:
         int(code)
         if len(code) == 4:
@@ -47,9 +90,21 @@ def validate_code(player, code):
 
 
 def use_padlock(player, code):
+    """
+    Check whether the passed code matches with an integer. If so,
+    call the Player.end_game() class method.
+
+    Otherwise, print a statement to inform the player their
+    input was incorrect and call the Player.get_action() class method.
+
+    Returns
+    -------
+    None
+    """
+
     if int(code) == 3791:
         player.end_game()
     else:
         print('''You enter the numbers into the padlock,\n
         but it does not budge.\n''')
-        player.start_game()
+        player.get_action()
